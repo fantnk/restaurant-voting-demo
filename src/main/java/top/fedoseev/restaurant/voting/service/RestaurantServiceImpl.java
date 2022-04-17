12f7a331495b7ef3d5b1517bfbf6ce05928e74fa
Aleksand Fedoseev <a@fedoseev.top>
2022-04-17 11:13:13 +0300

    extract RestaurantService interface
    Signed-off-by: Aleksand Fedoseev <a@fedoseev.top>

---
+++ src/main/java/top/fedoseev/restaurant/voting/service/RestaurantServiceImpl.java
@@ -0,0 +1,61 @@
+package top.fedoseev.restaurant.voting.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import top.fedoseev.restaurant.voting.exception.EntityNotFoundException;
+import top.fedoseev.restaurant.voting.exception.ErrorMessage;
+import top.fedoseev.restaurant.voting.mapper.RestaurantMapper;
+import top.fedoseev.restaurant.voting.model.Restaurant;
+import top.fedoseev.restaurant.voting.repository.RestaurantRepository;
+import top.fedoseev.restaurant.voting.to.RestaurantCreationRequest;
+import top.fedoseev.restaurant.voting.to.RestaurantResponse;
+
+import java.util.List;
+
+@Service
+@CacheConfig(cacheNames = RestaurantServiceImpl.CACHE_NAME)
+@RequiredArgsConstructor
+@SuppressWarnings("ClassCanBeRecord")
+public class RestaurantServiceImpl implements RestaurantService {
+    static final String CACHE_NAME = "restaurants";
+
+    private final RestaurantRepository repository;
+    private final RestaurantMapper restaurantMapper;
+
+    @Override
+    @Cacheable
+    public RestaurantResponse getById(Integer id) {
+        return repository.findById(id)
+                .map(restaurantMapper::toRestaurantResponse)
+                .orElseThrow(() -> new EntityNotFoundException(ErrorMessage.RESTAURANT_NOT_FOUND, "id", id));
+    }
+
+    @Override
+    @Transactional
+    @CacheEvict(value = CACHE_NAME, allEntries = true)
+    public RestaurantResponse create(RestaurantCreationRequest request) {
+        Restaurant restaurant = restaurantMapper.fromCreationRequest(request);
+        Restaurant savedRestaurant = repository.save(restaurant);
+        return restaurantMapper.toRestaurantResponse(savedRestaurant);
+    }
+
+    @Override
+    @Cacheable
+    public List<RestaurantResponse> findAll() {
+        return repository.findAll().stream()
+                .map(restaurantMapper::toRestaurantResponse)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    @CacheEvict(value = CACHE_NAME, allEntries = true)
+    public void delete(Integer id) {
+        repository.deleteExisted(id);
+    }
+
+}
